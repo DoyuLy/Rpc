94994ff1b7efce53ff17fcaabc3b522e1286bc35
DoyuLy <duyud@qq.com>
2016-11-06 17:30:54 +0800

    change file name

--- src/org/doyo/dynamic/DynaProxyHello.java
+++ src/org/doyo/dynamic/DynaProxyHello.java
@@ -6,34 +6,34 @@ import java.lang.reflect.Proxy;
 
 public class DynaProxyHello implements InvocationHandler {
 
-	private Object proxy;
+	private Object operater;
 
-	private Object delegate;
+	private Object origin;
 
 	public Object bind(Object delegate, Object proxy) {
 
-		this.proxy = proxy;
-		this.delegate = delegate;
-		return Proxy.newProxyInstance(this.delegate.getClass().getClassLoader(),
-				this.delegate.getClass().getInterfaces(), this);
+		this.operater = proxy;
+		this.origin = delegate;
+		return Proxy.newProxyInstance(this.origin.getClass().getClassLoader(),
+				this.origin.getClass().getInterfaces(), this);
 	}
 
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		Object result = null;
 		try {
-			// 反射得到操作者的实例
-			Class clazz = this.proxy.getClass();
+			// 反射得到操作者的实例 operation
+			Class clazz = this.operater.getClass();
 			// 反射得到操作者的Start方法
 			Method start = clazz.getDeclaredMethod("start", new Class[] { Method.class });
 			// 反射执行start方法
-			start.invoke(this.proxy, new Object[] { method });
+			start.invoke(this.operater, new Object[] { method });
 			// 执行要处理对象的原本方法
-			result = method.invoke(this.delegate, args);
+			result = method.invoke(this.origin, args);
 			// 反射得到操作者的end方法
 			Method end = clazz.getDeclaredMethod("end", new Class[] { Method.class });
 			// 反射执行end方法
-			end.invoke(this.proxy, new Object[] { method });
+			end.invoke(this.operater, new Object[] { method });
 
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/org/doyo/dynamic/Hello.java
+++ src/org/doyo/dynamic/Hello.java
@@ -2,7 +2,7 @@ package org.doyo.dynamic;
 
 public class Hello implements IHello {
 	public void sayHello(String name) {
-		System.out.println("Hello " + name);
+		System.out.println(name + " Hello!");
 	}
 
 	public void sayGoogBye(String name) {
